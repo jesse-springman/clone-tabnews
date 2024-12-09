30bb5406a9089138e444de85013a4a2f986b699e
Jessé Springman <134652811+jesse-springman@users.noreply.github.com>
2024-12-09 21:32:50 +0000

    att da msg

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
-const  Home =() =>{
-    return <h1>Te amo Gabriela Calil</h1>
+const Home = () => {
+    return (
+        <div>
+
+            <h1>OBRIDADO MEU DEUS, POR +1 DIA LINDO CONCEDIDO.</h1>
+
+            <p>
+                OBRIDADO MEU DEUS, POR +1 DIA LINDO CONCEDIDO.
+            </p>
+
+        </div>
+
+    )
 };
 
 export default Home;
