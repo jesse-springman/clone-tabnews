03d45713e1969190cca0cb66c6afea6687846b91
Jessé Springman <134652811+jesse-springman@users.noreply.github.com>
2025-01-15 22:46:33 +0000

    add folder infra and config compose.yaml

--- test/api/v1/status/get.test.js
+++ test/api/v1/status/get.test.js
@@ -1,5 +1,7 @@
-test("api/v1/status should return 200", async () => {
+test("api/v1/status should 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+
+  console.log(response);
 
   expect(response.status).toBe(200);
 });
