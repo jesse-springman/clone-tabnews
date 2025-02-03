8f9c0d4b4cee110f8338af97dfd76ccf167d6757
Jessé Springman <134652811+jesse-springman@users.noreply.github.com>
2025-02-03 22:34:32 +0000

    finish endpoint api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,37 @@
-import database from "infra/database.js";
+import databaseDev from "infra/database.js";
 
 async function status(request, response) {
-  const resultado = await database.query("SELECT 1 + 1 as sum;");
+  const uptadeAlt = new Date().toISOString();
 
-  response.status(200).json({ NOME: ` ${resultado}` });
+  const dataMaxConnect = await databaseDev.query("SHOW max_connections;");
+
+  let max_connections = parseInt(dataMaxConnect.rows[0].max_connections);
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const dataOpenConnect = await databaseDev.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  let opened_connections = dataOpenConnect.rows[0].count;
+
+  console.log(opened_connections);
+
+  const dataVersionPostgres = await databaseDev.query("SHOW server_version;");
+
+  const version = dataVersionPostgres.rows[0].server_version;
+
+  response.status(200).json({
+    uptade_alt: uptadeAlt,
+    dependencies: {
+      database: {
+        max_connections,
+        opened_connections,
+        version,
+      },
+    },
+  });
 }
 
 export default status;
--- test/integration/api/v1/get.test.js
+++ test/integration/api/v1/get.test.js
@@ -2,4 +2,20 @@ test("api/v1/status should 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
 
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const dateParsed = new Date(responseBody.uptade_alt).toISOString();
+
+  expect(dateParsed).toEqual(responseBody.uptade_alt);
+
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+
+  expect(responseBody.dependencies.database.opened_connections).toBeDefined();
+
+  expect(responseBody.dependencies.database.version).toBe("16.6");
+});
+
+test("test query parametres dinamic", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
 });
