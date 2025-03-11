19d2bd472db0eedb5132a47da21ab701ebfdff73
Jessé Springman <134652811+jesse-springman@users.noreply.github.com>
2025-03-11 21:58:50 +0000

    teste

--- test/integration/api/v1/migrations/delete.test.js
+++
@@ -1,7 +0,0 @@
-test("DELETE api/v1/migrations delete ", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "DELETE",
-  });
-
-  expect(response.status).toBe(204);
-});
