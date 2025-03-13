4ccb52ca19735722b537e2e97493ffda90eb7c60
Jessé Springman <134652811+jesse-springman@users.noreply.github.com>
2025-03-13 22:07:21 +0000

    working on the bug

--- test/integration/api/v1/migrations/delete.test.js
+++ test/integration/api/v1/migrations/delete.test.js
@@ -1,5 +1,7 @@
-test("GET api/v1/migrations should 200", async () => {
+test("DELETE api/v1/migrations", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "DELETE",
   });
+
+  expect(response.status).toBe(200);
 });
