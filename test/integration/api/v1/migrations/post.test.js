699c849b6df914b99639359a666ca46268928746
Jessé Springman <134652811+jesse-springman@users.noreply.github.com>
2025-05-16 21:44:44 +0000

    test: rename all descriptions test to use new  pattern

--- test/integration/api/v1/migrations/post.test.js
+++ test/integration/api/v1/migrations/post.test.js
@@ -1,37 +1,48 @@
-import database from "infra/database";
 import orchestrator from "test/orchestrator";
 
 beforeAll(async () => {
   await orchestrator.waitAllServices();
-  await database.query("drop schema public cascade; create schema public");
+  await orchestrator.databaseClean();
 });
 
-test("POST migration pending", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST to api/v1/migrations", () => {
+  describe("Anonymuos User", () => {
+    describe("Running pending migrations", () => {
+      test("For first time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
+        const responseBody = await response.json();
+
+        expect(response.status).toBe(201);
+
+        //validação para se o body in request é um array
+        expect(Array.isArray(responseBody)).toBe(true);
+
+        //migration pendente ocupa posição no array
+        expect(responseBody.length).toBeGreaterThan(0);
+      });
+
+      test("For second time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
+        expect(response.status).toBe(200);
+        const responseBody = await response.json();
+
+        //migration Implementada o array fica vazio
+        expect(responseBody.length).toBe(0);
+
+        // console.log(resultData);
+      });
+    });
   });
-
-  const responseBody = await response.json();
-
-  expect(response.status).toBe(201);
-
-  //validação para se o body in request é um array
-  expect(Array.isArray(responseBody)).toBe(true);
-
-  //migration pendente ocupa posição no array
-  expect(responseBody.length).toBeGreaterThan(0);
-});
-
-test("POST migration implementada", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-
-  expect(response.status).toBe(200);
-  const responseBody = await response.json();
-
-  //migration Implementada o array fica vazio
-  expect(responseBody.length).toBe(0);
-
-  // console.log(resultData);
 });
--- test/orchestrator.js
+++ test/orchestrator.js
@@ -1,4 +1,5 @@
 import retry from "async-retry";
+import database from "infra/database";
 
 async function waitAllServices() {
   await waitForWebServer();
@@ -19,8 +20,13 @@ async function waitAllServices() {
   }
 }
 
+async function databaseClean() {
+  await database.query("drop schema public cascade; create schema public");
+}
+
 const orchestrator = {
   waitAllServices,
+  databaseClean,
 };
 
 export default orchestrator;
