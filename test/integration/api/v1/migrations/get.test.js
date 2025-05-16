3bd4d1f5cb4850611065e6823e2b4ed62df23b3f
Jessé Springman <134652811+jesse-springman@users.noreply.github.com>
2025-05-16 21:55:54 +0000

    refactor: add `databaseClear()` in tests

--- test/integration/api/v1/migrations/get.test.js
+++ test/integration/api/v1/migrations/get.test.js
@@ -5,20 +5,17 @@ beforeAll(async () => {
   await orchestrator.databaseClean();
 });
 
-
-describe("GET api/v1/migrations", ()=>{
-  describe("Anonymuos User", ()=>{
+describe("GET api/v1/migrations", () => {
+  describe("Anonymuos User", () => {
     test("Retrieving pending migrations", async () => {
-     const response = await fetch("http://localhost:3000/api/v1/migrations");
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
 
       expect(response.status).toBe(200);
-
 
       const responseBody = await response.json();
 
       expect(Array.isArray(responseBody)).toBe(true);
       expect(responseBody.length).toBeGreaterThan(0);
-        });
-      });
     });
-  
+  });
+});
