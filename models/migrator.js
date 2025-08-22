4ab2726b4a0c3deffa443b8afbc30a2d7d848b7b
Jessé Springman <134652811+jesse-springman@users.noreply.github.com>
2025-08-22 20:45:29 +0000

    refactor: move `logs` for not to fill the screen

---
+++ models/migrator.js
@@ -0,0 +1,59 @@
+import migrationRunner from "node-pg-migrate";
+import { resolve } from "node:path";
+import database from "infra/database";
+
+
+  const defalutMigrations = {
+    dryRun: true,
+    dir: resolve("infra", "migrations"),
+    direction: "up",
+    log:()=>{}, //função vazia para tunelar os "logs" do terminal e para de atrabalhar nos tests
+    migrationsTable: "pgmigrations",
+  };
+
+  let dbClient;
+
+
+async function pendingMigrations() {
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const migrationPending = await migrationRunner({
+      ...defalutMigrations,
+      dbClient,
+    });
+
+    return migrationPending
+  } 
+  
+
+  finally {
+    await dbClient?.end();
+  }
+}
+
+async function runMigrationPending(){
+
+    try {
+    dbClient = await database.getNewClient();
+
+    const migrationImplementada = await migrationRunner({
+      ...defalutMigrations,
+      dbClient,
+      dryRun: false,
+    });
+
+    return migrationImplementada
+
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const modelMigration = {
+  pendingMigrations,
+  runMigrationPending
+}
+
+export default modelMigration
